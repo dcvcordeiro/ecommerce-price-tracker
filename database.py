9772f975618740496d26eb7abd7cd1a349714fe6
dcvcordeiro <cordeiro.dcv@gmail.com>
2024-11-26 23:54:49 +0000

    Add database insertion methods

--- database.py
+++ database.py
@@ -20,24 +20,39 @@ def get_database_session():
 
     if not os.path.exists(db):
         Base.metadata.create_all(engine)
-        
-    SessionDB = sessionmaker(bind=engine)
-
-    session_db = SessionDB()
-
-    # Check if the Platform table is empty and if it is populate 
-    row_count = session_db.query(Platform).count()
-    if row_count == 0:
-        add_platforms(db_session)
-
-    return db_session
-
-if "__main__" == __name__: 
-    # 1. Define DB if not defined
-
-    #add_platforms(db_session)
-    #query_plaform(db_session))
-    db_session = get_database_session()
 
 
+def add_product(session_db, product_name):
+    product = session_db.query(Product).filter_by(name=product_name).first()
     
+    if not product:
+        product = Product(name=product_name)
+        session_db.add(product)
+        session_db.commit()
+
+    return product.id
+    
+def add_platform(session_db, platform_name):
+    platform = session_db.query(Platform).filter_by(name=platform_name).first()
+    
+    if not platform:
+        platform = Platform(name=platform_name)
+        session_db.add(platform)
+        session_db.commit()
+
+    return platform.id
+
+def add_map_product_platform(session_db, product_id, platform_id, url):
+    mapping = session_db.query(ProductPlatform).filter_by(product_id=product_id, platform_id=platform_id).first()
+
+    if not mapping:
+        mapping = ProductPlatform(product_id=product_id, platform_id=platform_id, url=url)
+        session_db.add(mapping)
+    else:
+        mapping.url = url
+    session_db.commit()
+
+def add_price(session_db, mapping_id, product_price):
+    price_row = PriceHistory(product_platform_id = mapping_id, price=product_price)
+    session_db.add(price_row)
+    session_db.commit()
